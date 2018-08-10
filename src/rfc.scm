163a9a54844f004e39c0c4a7a0f8e79a4971c8b5
erkin <erkinbatu@gmail.com>
2018-08-10 04:30:07 +0300

    Name colours

--- src/rfc.scm
+++ src/rfc.scm
@@ -3,6 +3,15 @@
 (require-extension srfi-13)
 (require-extension srfi-18)
 
+(define-constant FRAME_COLOUR   1)
+(define-constant DECOR_COLOUR   2)
+(define-constant CHICKEN_COLOUR 3)
+(define-constant MESSAGE_COLOUR 4)
+(define-constant HELP_COLOUR    5)
+(define-constant CONTROL_COLOUR 6)
+(define-constant EXTRA_COLOUR   7)
+(define-constant ROBOT_COLOUR   8)
+
 (define robot-row (make-parameter 1))
 (define robot-col (make-parameter 1))
 
@@ -16,7 +25,7 @@
 
 (define (quit message code)
   (flushinp) ; Stop input to avoid cluttering the terminal
-  (attroff (A_BOLD))
+  (attroff A_BOLD)
   (clear)
   (endwin)
   (unless (zero? code)
@@ -47,15 +56,15 @@
   (+ 1 (random (- (COLS) 2))))
 
 (define (draw-chicken)
-  (attron (COLOR_PAIR 3))
+  (attron (COLOR_PAIR CHICKEN_COLOUR))
   (mvaddch (chicken-row) (chicken-col) (ACS_CKBOARD))
-  (attroff (COLOR_PAIR 3)))
+  (attroff (COLOR_PAIR CHICKEN_COLOUR)))
 
 (define (draw-robot)
   (mvaddch (robot-row-prev) (robot-col-prev) #\ ) ; remove the previous robot
-  (attron (COLOR_PAIR 7))
+  (attron (COLOR_PAIR ROBOT_COLOUR))
   (mvaddch (robot-row) (robot-col) (ACS_DIAMOND))
-  (attroff (COLOR_PAIR 7))
+  (attroff (COLOR_PAIR ROBOT_COLOUR))
   (moved? #f))
 
 (define (move-robot #!key (v 'nil) (h 'nil))
@@ -91,42 +100,42 @@
   (moved? #t))
 
 (define (rfc-frame)
-  (attron (COLOR_PAIR 1))
+  (attron (COLOR_PAIR FRAME_COLOUR))
   (box (stdscr) (ACS_VLINE) (ACS_HLINE))
   (mvhline 2 1 (ACS_HLINE) (- (COLS) 2))
   (mvaddch 2 0 (ACS_LTEE))
   (mvaddch 2 (sub1 (COLS)) (ACS_RTEE))
-  (attroff (COLOR_PAIR 1))
+  (attroff (COLOR_PAIR FRAME_COLOUR))
 
-  (attron (COLOR_PAIR 2))
+  (attron (COLOR_PAIR DECOR_COLOUR))
   (mvprintw 2 2 "robotfinds")
-  (attroff (COLOR_PAIR 2))
+  (attroff (COLOR_PAIR DECOR_COLOUR))
 
-  (attron (COLOR_PAIR 3))
+  (attron (COLOR_PAIR CHICKEN_COLOUR))
   (printw "chicken")
-  (attroff (COLOR_PAIR 3)))
+  (attroff (COLOR_PAIR CHICKEN_COLOUR)))
 
 (define (rfc-splash)
   (rfc-frame)
   ;; The ordering is important to avoid rewriting
   ;; some parts with spaces.
 
-  (attron (COLOR_PAIR 6))
+  (attron (COLOR_PAIR HELP_COLOUR))
   (mvprintw (- (LINES) 2) (- (COLS) 18) "Press Q to exit.")
   (mvprintw (- (LINES) 3) 2 "Use       to move.")
   (mvprintw 3 3 "Press any key to begin/resume.")
-  (attroff (COLOR_PAIR 6))
+  (attroff (COLOR_PAIR HELP_COLOUR))
   
-  (attron (COLOR_PAIR 5))
+  (attron (COLOR_PAIR CONTROL_COLOUR))
   (mvprintw (- (LINES) 4) 6 "7 8 9")
   (mvprintw (- (LINES) 3) 6 "4   6")
   (mvprintw (- (LINES) 2) 6 "1 2 3")
-  (attroff (COLOR_PAIR 5))
+  (attroff (COLOR_PAIR CONTROL_COLOUR))
 
-  (attron (COLOR_PAIR 4))
+  (attron (COLOR_PAIR MESSAGE_COLOUR))
   (centre-message "You are the Robot!" "Find the Chicken!" "Godspeed!")
   (mvaddch (- (LINES) 3) 8 (ACS_PLUS))
-  (attroff (COLOR_PAIR 4))
+  (attroff (COLOR_PAIR MESSAGE_COLOUR))
     
   (if (member (getch) '(#\q #\Q KEY_F0)) ; Mock the player for
       (quit "That was quick." 0)) ; exiting on help screen.
@@ -144,13 +153,15 @@
       (quit "Your terminal does not support colours." 1))
   
   (start_color)
-  (init_pair 1 COLOR_BLUE COLOR_BLACK)
-  (init_pair 2 COLOR_CYAN COLOR_BLACK)
-  (init_pair 3 COLOR_RED COLOR_BLACK)
-  (init_pair 4 COLOR_YELLOW COLOR_BLACK)
-  (init_pair 5 COLOR_MAGENTA COLOR_BLACK)
-  (init_pair 6 COLOR_GREEN COLOR_BLACK)
-  (init_pair 7 COLOR_GREEN COLOR_WHITE)
+  (init_pair FRAME_COLOUR COLOR_BLUE COLOR_BLACK)
+  (init_pair DECOR_COLOUR COLOR_CYAN COLOR_BLACK)
+  (init_pair CHICKEN_COLOUR COLOR_RED COLOR_BLACK)
+  (init_pair MESSAGE_COLOUR COLOR_YELLOW COLOR_BLACK)
+  (init_pair HELP_COLOUR COLOR_GREEN COLOR_BLACK)
+  (init_pair CONTROL_COLOUR COLOR_MAGENTA COLOR_BLACK)
+  (init_pair EXTRA_COLOUR COLOR_BLUE COLOR_BLACK)
+  (init_pair ROBOT_COLOUR COLOR_GREEN COLOR_WHITE)
+  
 
   (raw) (noecho)
   (keypad (stdscr) #t)
@@ -232,9 +243,9 @@
      (refresh))
    6)
   
-  (attron (COLOR_PAIR 4))
+  (attron (COLOR_PAIR MESSAGE_COLOUR))
   (centre-message "You found the chicken!" "Bravo!")
-  (attroff (COLOR_PAIR 4))
+  (attroff (COLOR_PAIR MESSAGE_COLOUR))
 
   (refresh)
   (thread-sleep! 1)
